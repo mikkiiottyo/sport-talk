ebe4bb1e83506f692f19749d9c3b60b4a8493d73
mikkiiottyo <michaelaguirre15@gmail.com>
2025-05-09 13:29:49 -0800

    update index/auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,29 @@
 import express from 'express'
+import User from '../models/User.js'
+import bcrypt from 'bcrypt'
 
 const router = express.Router()
 
 router.post('/register', async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+        const user = await User.findOne({email})
+        if(user) {
+            return res.status(401).json({success: false, message:"User already taken"})
+        }
 
+        const hashpassword = await bcrypt.hash(password, 10)
+
+        const newUser = new User({
+            name, email, password: hashpassword
+        })
+
+        await newUser.save()
+
+        return res(200).json({success: true, message:"Account Created Successfully"})
+    }catch(error) {
+        return res(500).json({success: true, message:"Account Created Failed"})
+    }
 })
 
 export default router
