8c10acf63c436669da2ec78845eadd7944b08bb3
mikkiiottyo <michaelaguirre15@gmail.com>
2025-05-08 21:02:02 -0800

    Update signup.jsx

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -2,23 +2,25 @@ import React from 'react'
 
 const signup = () => {
   return (
-    <div>
-      <h2>Sign Up</h2>
+    <div className="flex jusify-center items-center min-h-screen bg-gray-100">
+        <div className="border shadow p-6 w-80 bg-white">
+      <h2 className="text-2x1 font-bold mb-4">Sign Up</h2>
       <form>
-        <div>
-            <label htmlFor="name">Name</label>
-            <input type="text" placeholder='Enter Name' required />
+        <div className="mb-4">
+            <label classname="block text-grey-700">Name</label>
+            <input type="text" classname="w-full px-3 py-2 border" placeholder='Enter Username' required />
         </div>
-        <div>
-            <label htmlFor="email">Email</label>
-            <input type="email" placeholder='Enter Email' required />
+        <div className="mb-4">
+            <label classname="block text-grey-700">Email</label>
+            <input type="email"  classname="w-full px-3 py-2 border" placeholder='Enter Email' required />
         </div>
-        <div>
-            <label htmlFor="Password">Password</label>
-            <input type="password" placeholder='*******' required />
+        <div className="mb-4">
+            <label  classname="block text-grey-700">Password</label>
+            <input type="password"  classname="w-full px-3 py-2 border" placeholder='*******' required />
         </div>
         <button>Sing Up</button>
       </form>
+      </div>
     </div>
   )
 }
