af5f151b6cde36c5cfa047a75c415bba2a1d6864
mikkiiottyo <michaelaguirre15@gmail.com>
2025-05-08 21:28:03 -0800

    Update signup.jsx

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -1,15 +1,29 @@
 import React, { useState } from 'react';
+import axios from 'axios'
 
 const Signup = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+        const response = await axios.post(
+            'http://localhost:5000/api/auth/register',
+             {name, email, password}
+            );
+            console.log(response)
+    } catch(error) {
+        console.log(error)
+    }
+  }
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="border shadow p-6 w-80 bg-white">
         <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block text-gray-700">Name</label>
             <input
