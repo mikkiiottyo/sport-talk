3c2ce205cec8a224c055223488c3632f65ccb3f2
mikkiiottyo <michaelaguirre15@gmail.com>
2025-05-09 14:27:21 -0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import Signup from "./pages/signup"
-import {BrowserRouter, Routes, Route} from 'react' 
+import {BrowserRouter, Routes, Route} from 'react-router-dom' 
 import Home from './pages/Home'
 import Login from "./pages/Login"
 
