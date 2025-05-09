f56b556866bb1d86508a354cfe1de5b4f36a72ac
mikkiiottyo <michaelaguirre15@gmail.com>
2025-05-08 21:18:48 -0800

    Update signup.jsx

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -1,33 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Signup = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div className="flex jusify-center items-center min-h-screen bg-gray-100">
-        <div className="border shadow p-6 w-80 bg-white">
-      <h2 className="text-2x1 font-bold mb-4">Sign Up</h2>
-      <form>
-        <div className="mb-4">
-            <label classname="block text-grey-700">Name</label>
-            <input type="text" classname="w-full px-3 py-2 border" placeholder='Enter Username' required />
-        </div>
-        <div className="mb-4">
-            <label classname="block text-grey-700">Email</label>
-            <input type="email"  classname="w-full px-3 py-2 border" placeholder='Enter Email' required />
-        </div>
-        <div className="mb-4">
-            <label  classname="block text-grey-700">Password</label>
-            <input type="password"  classname="w-full px-3 py-2 border" placeholder='*******' required />
-        </div>
-        <div className="mb-4">
-        <button type="submit" className="w-full bg-teal-600 text-white py-2 ">Sing Up</button>
-        <p className="text-center">
-            Already have an Account? <a href="">Login</a>
-        </p>
-        </div>
-      </form>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="border shadow p-6 w-80 bg-white">
+        <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
+        <form>
+          <div className="mb-4">
+            <label className="block text-gray-700">Name</label>
+            <input
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              className="w-full px-3 py-2 border"
+              placeholder="Enter Username"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Email</label>
+            <input
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-3 py-2 border"
+              placeholder="Enter Email"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Password</label>
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-3 py-2 border"
+              placeholder="*******"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <button type="submit" className="w-full bg-teal-600 text-white py-2">
+              Sign Up
+            </button>
+            <p className="text-center mt-2">
+              Already have an Account? <a href="#">Login</a>
+            </p>
+          </div>
+        </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
