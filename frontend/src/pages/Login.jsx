c14f19ecfed8a2959d7406460e5ab1191c191afd
mikkiiottyo <michaelaguirre15@gmail.com>
2025-05-09 15:25:55 -0800

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,7 +5,7 @@ import { Link, useNavigate } from 'react-router-dom';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault()
