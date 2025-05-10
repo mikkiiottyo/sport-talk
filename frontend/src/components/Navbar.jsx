0953470468c69fd868b9c8568b6c10954cab47e3
mikkiiottyo <michaelaguirre15@gmail.com>
2025-05-09 16:23:38 -0800

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
+   <nav>
     <div>
-      
+        <Link to="/">SportTalk</Link>
     </div>
-  )
-}
+    <input
+    type="text"
+    placeholder="Questions Asked"
+    className="bg-gray-600 px-4 py-2 rounded"
+     />
+     <div>
+        <span className="mr-4">user name</span>
+        <Link to="/login" className="bg-blue-500 px-4 py-2 rounded mr-4">
+        Login
+        </Link>
+        <Link to="/register" className="bg-green-500 px-4 py-2 rounded mr-4">
+        Signup
+        </Link>
+        <button className="bg-red-500 px-4 py-2 rounded">Logout</button>
+     </div>
+   </nav>
+  );
+};
 
 export default Navbar
