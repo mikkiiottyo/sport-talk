8e56510f2429d526c14a936c5efe211cf2bdd442
mikkiiottyo <michaelaguirre15@gmail.com>
2025-05-12 17:03:08 -0800

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="sidebar">
+            <ul>
+                <li><Link to="/questions">Questions</Link></li>
+                <li><Link to="/answers">Answers</Link></li>
+            </ul>
+        </div>
+    );
 }
 
-export default Sidebar
+export default Sidebar;
