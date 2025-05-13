8b0d87f8bebef0a5707a3e77be5228f56c9a460a
mikkiiottyo <michaelaguirre15@gmail.com>
2025-05-12 17:26:34 -0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,38 @@ import Signup from "./pages/signup"
 import {BrowserRouter, Routes, Route} from 'react-router-dom' 
 import Home from './pages/Home'
 import Login from "./pages/Login"
+import Sidebar from "./components/Sidebar"
+
+
+
+
+function DashboardLayout({ children }) {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="ml-64 p-6 w-full">
+        {children}
+      </div>
+    </div>
+  );
+}
+
 
 function App() {
   
 
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path ="/" element={<Home /> }></Route>
-      <Route path ="/register" element={<Signup /> }></Route>
-      <Route path ="/login" element={<Login /> }></Route>
-    </Routes>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/register" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        
+        {/* Routes with Sidebar */}
+        <Route path="/" element={<DashboardLayout><Home /></DashboardLayout>} />
+        <Route path="/questions" element={<DashboardLayout><QuestionsPage /></DashboardLayout>} />
+        <Route path="/answers" element={<DashboardLayout><AnswersPage /></DashboardLayout>} />
+      </Routes>
     </BrowserRouter>
   )
 }
