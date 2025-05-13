01ef35c1c17560dfb9331e2224bb2d8125d4e550
mikkiiottyo <michaelaguirre15@gmail.com>
2025-05-12 17:04:33 -0800

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            <ul>
-                <li><Link to="/questions">Questions</Link></li>
-                <li><Link to="/answers">Answers</Link></li>
-            </ul>
-        </div>
-    );
-}
+  return (
+    <div className="w-64 h-screen bg-gray-800 text-white p-4 fixed">
+      <h2 className="text-2xl font-bold mb-6">SportTalk</h2>
+      <ul className="space-y-4">
+        <li>
+          <Link to="/questions" className="block hover:bg-gray-700 p-2 rounded">
+            Questions
+          </Link>
+        </li>
+        <li>
+          <Link to="/answers" className="block hover:bg-gray-700 p-2 rounded">
+            Answers
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Sidebar;
