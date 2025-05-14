a383d171f20c9429da481502919fb87e73525a04
mikkiiottyo <michaelaguirre15@gmail.com>
2025-05-13 19:49:21 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import connectToMongooseDB from './db/db.js'
-
+import questionRouter from './routes/questions.js';
+import answerRouter from './routes/answers.js';
 import authRouter from './routes/auth.js'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api/auth', authRouter)
+app.use('/api/questions', questionRouter)
+app.use('/api/answers', answerRouter)
 
 app.listen(5000, () => {
     connectToMongooseDB()
