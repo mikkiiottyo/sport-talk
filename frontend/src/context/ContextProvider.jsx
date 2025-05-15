48ebf7ed34c89323e4b9ad3db2d79ee90b22ad21
mikkiiottyo <michaelaguirre15@gmail.com>
2025-05-14 19:26:55 -0800

    Update ContextProvider.jsx

--- frontend/src/context/ContextProvider.jsx
+++ frontend/src/context/ContextProvider.jsx
@@ -16,6 +16,7 @@ const ContextProvider = ({children}) => {
     const logout = () => {
         setUser(null);
         localStorage.removeItem('user');
+         localStorage.removeItem('token');
       };
   return (
     <div>
