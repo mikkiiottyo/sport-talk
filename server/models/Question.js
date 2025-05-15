9e1f9ddac5063c7a7bb322f40e6315eb3389c136
mikkiiottyo <michaelaguirre15@gmail.com>
2025-05-14 19:14:07 -0800

    Update Question.js

--- server/models/Question.js
+++ server/models/Question.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const QuestionSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true },
+  description: { type: String },
   category: String,
   subcategory: String,
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
